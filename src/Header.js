02f5a7016219c3d68a773adf8a1a8831f08d53b9
Mike <samson9565@gmail.com>
2024-12-01 00:42:03 +0800

    without shortcut

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Header() {
     return (
         <div style={{alignSelf: "center", color: "#ccc"}}>
-            Expenses record tools - integrated with iOS Shortcuts - <b>v3.4.1</b>
+            Expenses record tools <b>v3.5.0</b>
         </div>
     );
 }
