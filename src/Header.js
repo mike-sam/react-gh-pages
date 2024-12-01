9d19eab63972c13c415e783edca04b445c92c4d2
Mike <samson9565@gmail.com>
2024-12-01 22:58:11 +0800

    styling

--- src/Header.js
+++ src/Header.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 function Header() {
     return (
-        <div style={{alignSelf: "center", color: "#ccc"}}>
-            Expenses record tools <b>v3.5.0</b>
+        <div style={{alignSelf: "center", color: "#ccc", margin: "10px 0px", width: "100%"}}>
+            <span style={{display:"flex",justifyContent:"center"}}>Expenses record tools <b style={{marginLeft:"5px"}}>v3.5.0</b></span>
         </div>
     );
 }
