a410386a3a6181bd68880245ef04897f6a353505
Mike <samson9565@gmail.com>
2024-12-03 18:29:12 +0800

    versioning

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Header() {
     return (
         <div style={{alignSelf: "center", color: "#ccc", margin: "10px 0px", width: "100%"}}>
-            <span style={{display:"flex",justifyContent:"center"}}>Expenses record tools <b style={{marginLeft:"5px"}}>v3.5.0</b></span>
+            <span style={{display:"flex",justifyContent:"center"}}>Expenses record tools <b style={{marginLeft:"5px"}}>v3.5.1</b></span>
         </div>
     );
 }
